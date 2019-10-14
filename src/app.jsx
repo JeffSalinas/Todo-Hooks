2307a6823f3526cd6488ebd33901c279d5d89b4e
Jeffrey Salinas <jeff.salinas.music@gmail.com>
2019-10-13 23:41:26 -0500

    Create todo hook and todo component

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,28 @@
-import React, {Component} from 'react';
+import React, { useState } from 'react';
 
-class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {};
-    }
-    render() {
-        return (
-            <div>Hello from app.jsx!!</div>
-        )
-    }
+const Todo = ({ todo }) => <div className="todo">{todo.text}</div>
+
+const App = () => {
+    const [ todos, setTodo ] = useState([
+        { text: 'First Thing' },
+        { text: 'Next Thing' },
+        { text: 'Third Thing' }
+    ]);
+
+
+    return (
+        <div className="app">
+            <div className="todo-list">
+                {todos.map((todo, index) => (
+                    <Todo
+                        key={index}
+                        index={index}
+                        todo={todo}
+                    />
+                ))}
+            </div>
+        </div>
+    )
 }
 
 export default App;
